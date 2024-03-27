2546858949e6097aa632a97c6d08cd0e50a61089
Ashish32700 <91833254+Ashish32700@users.noreply.github.com>
2024-03-28 02:44:21 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, request 
+
+app = Flask(__name__) 
+
+
+@app.route('/', methods=['GET']) 
+def helloworld(): 
+	if(request.method == 'GET'): 
+		return "hello"
+
+
