74a965c6df439c311cbcc325b94cd1c6320be408
Ashish32700 <91833254+Ashish32700@users.noreply.github.com>
2024-03-28 02:50:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET']) 
 def helloworld(): 
-	if(request.method == 'GET'): 
-		return "hello, ashish this is your first deploy!"
+	return "hello, ashish this is your first deploy!"
 
 
